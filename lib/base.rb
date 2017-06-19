d0b90a7f6fe79e2b399a963f2b843a8452c9d41e
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2017-06-19 08:38:55 -0400

    Add herokuish dockerfile creation

--- lib/base.rb
+++ lib/base.rb
@@ -1,5 +1,7 @@
 ##!/usr/bin/ruby
 
+BASE_SCRIPT_PATH = "https://raw.githubusercontent.com/benjamincaldwell/ci-scripts/master/"
+
 def command(*options)
   puts("\x1b[34m#{options.join " "}\x1b[0m")
   t = Time.now
@@ -21,6 +23,23 @@ def required_env(key)
   end
 end
 
+def from_env(key, default="")
+  if ENV[key]
+    ENV[key]
+  else
+    default
+  end
+end
+
 def run_script(path)
-  raise "Not implemented"
+  require 'net/http' unless defined? Net::HTTP
+  uri = URI.join(BASE_SCRIPT_PATH, path)
+  res = Net::HTTP.get_response(uri)
+
+  unless res.code == "200"
+    puts "Unable to fetch #{path}\n"
+    exit 1
+  end
+
+  eval(res.body)
 end
